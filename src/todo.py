c1f2ee917f2c6512e08844fdf9d4713028cb4e8e
jobin_nelson <jobinnelson369@gmail.com>
2024-12-20 17:58:56 +0530

    refactor(todo): rename todos to tasks

--- src/todo.py
+++ src/todo.py
@@ -79,7 +79,7 @@ class Task:
 class TodoState(NamedTuple):
     creds: Path
     projects: Path
-    todos: Path
+    tasks: Path
 
 
 class XdgDirs(StrEnum):
@@ -136,7 +136,7 @@ def compose2(f: Callable[[B], C], g: Callable[[A], B]) -> Callable[[A], C]:
 todoState = TodoState(
     creds=get_path(XdgDirs.DATA) / 'todoist' / 'creds.json',
     projects=get_path(XdgDirs.STATE) / 'todoist' / 'projects.json',
-    todos=get_path(XdgDirs.STATE) / 'todoist' / 'todos.json',
+    tasks=get_path(XdgDirs.STATE) / 'todoist' / 'tasks.json',
 )
 
 
@@ -255,7 +255,7 @@ def get_active_tasks() -> list[Task]:
         dict2Task(t)
         for t in get_object(
             'https://api.todoist.com/rest/v2/tasks',
-            todoState.todos,
+            todoState.tasks,
             ExitCode.TASK_NOT_FOUND,
         )
     ]
