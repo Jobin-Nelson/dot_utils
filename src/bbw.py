3784cba7c7a191c989042a0d3d966c73bbb4c400
jobin_nelson <jobinnelson369@gmail.com>
2025-08-21 15:33:57 +0530

    chore: bbw.py backup commit

--- src/bbw.py
+++ src/bbw.py
@@ -18,19 +18,7 @@ import asyncio
 import itertools
 import shutil
 from dataclasses import dataclass, field
-from functools import partial
-from itertools import pairwise
 from pathlib import Path
-from typing import Callable
-
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃                    Global Variables                      ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
-# Actual workers are NUM_WORKERS * 4 (add, commit, push, status, result)
-NUM_WORKERS = 5
-QUEUE_MAX_SIZE = 25
-
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 # ┃                        VCS Types                         ┃
@@ -43,9 +31,6 @@ class Git:
     git_dir: Path | None = None
     work_tree: Path | None = None
     cmd: list[str] = field(init=False)
-    proc: asyncio.subprocess.Process | None = field(
-        init=False, repr=False, default=None
-    )
 
     def __post_init__(self):
         self.cmd = ['git', '-C', str(self.cwd)]
@@ -56,7 +41,7 @@ class Git:
 
     async def add(self):
         add_args = ['add', '-u']
-        self.proc = await exec_cmd(self.cmd + add_args)
+        await exec_cmd(self.cmd + add_args)
 
     async def commit(self):
         commit_args = [
@@ -66,27 +51,31 @@ class Git:
             '-m',
             'chore: bbw.py backup commit',
         ]
-        self.proc = await exec_cmd(self.cmd + commit_args)
+        await exec_cmd(self.cmd + commit_args)
 
     async def push(self):
         push_args = ['push', 'origin', 'HEAD']
-        self.proc = await exec_cmd(self.cmd + push_args)
+        await exec_cmd(self.cmd + push_args)
 
     async def status(self):
         status_args = ['status', '--porcelain']
         if self.git_dir is None:
             status_args.append('--untracked-files=normal')
         cmd = self.cmd + status_args
-        self.proc = await exec_cmd(cmd, True)
+        stdout = await exec_cmd(cmd, True)
+        if stdout:
+            print(f'{self.cwd} repo is dirty')
 
 
-async def exec_cmd(cmd: list[str], cap_output: bool = False):
+async def exec_cmd(cmd: list[str], cap_output: bool = False) -> bytes:
     pipe = asyncio.subprocess.PIPE if cap_output else asyncio.subprocess.DEVNULL
-    return await asyncio.create_subprocess_exec(
+    proc = await asyncio.create_subprocess_exec(
         *cmd,
         stdout=pipe,
         stderr=asyncio.subprocess.DEVNULL,
     )
+    stdout, _ = await proc.communicate()
+    return stdout
 
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -94,77 +83,11 @@ async def exec_cmd(cmd: list[str], cap_output: bool = False):
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
 
-async def _producer(
-    tg: asyncio.TaskGroup,
-    batch: list[Git],
-    work_queue: asyncio.Queue,
-    producer_completed: asyncio.Event,
-):
-    for job in batch:
-        tg.create_task(work_queue.put(job))
-    producer_completed.set()
-
-
-async def _worker(
-    work_queue: asyncio.Queue, result_queue: asyncio.Queue, callback: Callable
-):
-    while True:
-        task = await work_queue.get()
-        if task.proc is not None:
-            await task.proc.wait()
-        await callback(task)()
-        await result_queue.put(task)
-        work_queue.task_done()
-
-
-async def _janitor(queue: asyncio.Queue):
-    while True:
-        task = await queue.get()
-        stdout, _ = await task.proc.communicate()
-        if stdout:
-            print(f'{task.cwd} repo is dirty')
-        queue.task_done()
-
-
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃                        Callbacks                         ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
-
-def add_callback(git: Git):
-    return git.add
-
-
-def commit_callback(git: Git):
-    return git.commit
-
-
-def push_callback(git: Git):
-    return git.push
-
-
-def status_callback(git: Git):
-    return git.status
-
-
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃                     Worker Handlers                      ┃
-# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-
-
-def hook_workers_handler(
-    tg: asyncio.TaskGroup,
-    in_queue: asyncio.Queue,
-    out_queue: asyncio.Queue,
-    callback: Callable,
-):
-    for _ in range(NUM_WORKERS):
-        tg.create_task(_worker(in_queue, out_queue, callback))
-
-
-def hook_cleanup_handler(tg: asyncio.TaskGroup, end_queue: asyncio.Queue):
-    for _ in range(NUM_WORKERS):
-        tg.create_task(_janitor(end_queue))
+async def worker(git: Git):
+    await git.add()
+    await git.commit()
+    await git.push()
+    await git.status()
 
 
 # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -191,43 +114,17 @@ def get_repos() -> list[Git]:
     ]
 
 
-async def _controller() -> int:
-    batch = get_repos()
-
-    queues = [
-        asyncio.Queue(maxsize=QUEUE_MAX_SIZE),  # add queue
-        asyncio.Queue(maxsize=QUEUE_MAX_SIZE),  # commit queue
-        asyncio.Queue(maxsize=QUEUE_MAX_SIZE),  # push queue
-        asyncio.Queue(maxsize=QUEUE_MAX_SIZE),  # status queue
-        asyncio.Queue(maxsize=QUEUE_MAX_SIZE),  # result queue
-    ]
-    callbacks = [add_callback, commit_callback, push_callback, status_callback]
-
-    producer_completed = asyncio.Event()
-    producer_completed.clear()
+async def controller():
+    repos = get_repos()
 
     async with asyncio.TaskGroup() as tg:
-        # load all repos to first queue
-        await _producer(tg, batch, queues[0], producer_completed)
-
-        hook_workers = partial(hook_workers_handler, tg)
-        for (in_queue, out_queue), callback in zip(
-            pairwise(queues), callbacks, strict=True
-        ):
-            hook_workers(in_queue, out_queue, callback)
-
-        # cleanup last queue
-        hook_cleanup_handler(tg, queues[-1])
-
-        await producer_completed.wait()
-        for queue in queues:
-            await queue.join()
-
-    return 0
+        for repo in repos:
+            tg.create_task(worker(repo))
 
 
 def main() -> int:
-    return asyncio.run(_controller())
+    asyncio.run(controller())
+    return 0
 
 
 def check_requirements():
